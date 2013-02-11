26e67a1d4c416cd58f9e2ffc519d94c1dabbf1d6
Barun <barunthapa.bvdt@gmail.com>
2013-02-11 22:08:31 +0545

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+POS::Application.config.secret_token = 'f26ac6c2bf22094933f430bde619efc5147c1aeb7281e4b5c32133313d5df40985606e3d8cb7e6a083972a06e8a41ad5b3d40c9ff8bcddd777554036d18bb71c'
